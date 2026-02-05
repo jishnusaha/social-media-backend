244c089d306e78942c9ba97fad37d1005b7c3e43
Jishnu Saha <jishnusaha89@gmail.com>
2026-02-05 14:45:52 +0600

    Create linux/amd64 image locally

--- update-image-registry.sh
+++ update-image-registry.sh
@@ -5,7 +5,7 @@ REPO=307946642170.dkr.ecr.eu-north-1.amazonaws.com/social-media-backend
 TAG=latest  # or dynamic: TAG=$(date +%Y%m%d%H%M%S)
 
 echo "Building Docker image..."
-docker build -t $REPO:$TAG .
+docker build --platform linux/amd64 -t $REPO:$TAG .
 
 echo "Logging into ECR..."
 aws ecr get-login-password --region eu-north-1 --profile personal \
